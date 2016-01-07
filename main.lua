5a5758a65fbe2366aba57dd1c73856d22103a7e4
Julio Lopez <ljuliom@gmail.com>
2016-01-06 23:13:07 -0500

    iterating table

--- main.lua
+++ main.lua
@@ -1,13 +1,10 @@
 function love.load()
-  MyGlobalTable = {}
-  -- MyGlobalTable[1] = 'My first element'
-  -- MyGlobalTable[2] = 'My second element'
-  MyGlobalTable = { 'My first element', 'My second element' }
-  -- Using Subtables
-  -- MyGlobalTable[1] = {}
-  -- MyGlobalTable[1][1] = 'Stuff inside a sub-table'
-  -- MyGlobalTable[1][2] = 'More stuff inside a sub-table'
+  Messages = { 'My first element', 'My second element' }
 end
+
 function love.draw()
-  love.graphics.print(MyGlobalTable,100,100) -- prints 'My first element' on 100,100
+  -- Using # returns the length of the table
+  for i=1,#Messages do
+    love.graphics.print(Messages[i], 100, 50 * i)
+  end
 end
