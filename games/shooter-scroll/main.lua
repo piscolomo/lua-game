e9c76f3f266862b7667765e4e1b8c0cbca6e246e
Julio Lopez <ljuliom@gmail.com>
2016-01-07 17:26:58 -0500

    Game1: lose and restart

--- games/shooter-scroll/main.lua
+++ games/shooter-scroll/main.lua
@@ -1,4 +1,16 @@
+-- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
+-- Returns true if two boxes overlap, false if they don't
+-- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
+-- x2,y2,w2 & h2 are the same, but for the second box
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
+
 Player = {x = 200, y = 710, speed = 150, img = nil}
+IsAlive = true
 
 -- Timers
 -- We declare these here so we don't have to edit them multiple places
@@ -22,6 +34,21 @@ function love.update(dt)
   -- Exit Button
   if love.keyboard.isDown('escape') then
     love.event.push('quit')
+  end
+
+  -- Restart Button
+  if not IsAlive and love.keyboard.isDown('r') then
+    -- Reset Game
+    Bullets = {}
+    Enemies = {}
+    Player.x = 200
+    Player.y = 710
+    IsAlive = true
+    CanShootTimerMax = 0.2 
+    CanShootTimer = CanShootTimerMax
+    CanShoot = true
+    EnemyTimerMax = 2
+    EnemyTimer = EnemyTimerMax
   end
 
   -- Airplane Movement
@@ -76,18 +103,29 @@ function love.update(dt)
     if enemy.y > 850 then -- remove enemy if is out of screen
       table.remove(Enemies, i)
     end
+    
+    -- Enemy Collision with Player
+    if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), Player.x, Player.y, Player.img:getWidth(), Player.img:getHeight()) and IsAlive then
+      table.remove(Enemies, i)
+      IsAlive = false
+    end  
   end
 end
 
 function love.draw()
   -- Drawing Airplane
-  love.graphics.draw(Player.img, Player.x, Player.y)
+  if IsAlive then
+    love.graphics.draw(Player.img, Player.x, Player.y)
+  else
+    love.graphics.print('Press R to Restart', love.graphics:getWidth()/2-50, love.graphics:getHeight()/2-10)
+  end
 
   -- Drawing Bullets
   for i,bullet in ipairs(Bullets) do
     love.graphics.draw(bullet.img, bullet.x, bullet.y)
   end
 
+  -- Drawing Enemies
   for i,enemy in ipairs(Enemies) do
     love.graphics.draw(enemy.img, enemy.x, enemy.y)
   end
