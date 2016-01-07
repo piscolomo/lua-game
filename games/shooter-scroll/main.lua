8d8fb5626a24e7608a3c26900f5dd2a45654a7ff
Julio Lopez <ljuliom@gmail.com>
2016-01-07 16:31:21 -0500

    Game1: Moving airplane

--- games/shooter-scroll/main.lua
+++ games/shooter-scroll/main.lua
@@ -1,13 +1,25 @@
-playerImg = nil
+Player = {x = 200, y = 710, speed = 150, img = nil}
 
 function love.load()
-  playerImg = love.graphics.newImage("assets/plane.png")
+  Player.img = love.graphics.newImage("assets/plane.png")
 end
 
-function love.update()
+function love.update(dt)
+  if love.keyboard.isDown('escape') then
+    love.event.push('quit')
+  end
 
+  if love.keyboard.isDown('left', 'a') then
+    if Player.x > 0 then -- ensure plane is always in window
+      Player.x = Player.x - (Player.speed*dt)
+    end
+  elseif love.keyboard.isDown('right', 'd') then
+    if Player.x < (love.graphics.getWidth() - Player.img:getWidth()) then
+      Player.x = Player.x + (Player.speed*dt)
+    end
+  end
 end
 
 function love.draw()
-  love.graphics.draw(playerImg, 100, 100)
+  love.graphics.draw(Player.img, Player.x, Player.y)
 end
