5e3b2b6e325602b067ec3752d6c7dadeaa128055
Julio Lopez <ljuliom@gmail.com>
2016-01-07 00:33:21 -0500

    using String matching for build the map

--- main.lua
+++ main.lua
@@ -5,46 +5,65 @@ function love.load()
   local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
 
   local quadInfo = {
-    {  0,  0 }, -- 1 = grass 
-    { 32,  0 }, -- 2 = box
-    {  0, 32 }, -- 3 = flowers
-    { 32, 32 }  -- 4 = boxTop
+    { ' ', 0,  0  }, -- grass 
+    { '#', 32,  0 }, -- box
+    { '*', 0, 32  }, -- flowers
+    { '^', 32, 32 }  -- boxTop
   }
   Quads = {}
-  for i,info in ipairs(quadInfo) do
-    -- info[1] = x, info[2] = y
-    Quads[i] = love.graphics.newQuad(info[1], info[2], TileW, TileH, tilesetW, tilesetH)
+  for _,info in ipairs(quadInfo) do
+    -- info[1] = character, info[2]= x, info[3] = y
+    Quads[info[1]] = love.graphics.newQuad(info[2], info[3], TileW, TileH, tilesetW, tilesetH)
   end
 
-  TileTable = {
-    { 4,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4 },
-    { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,4 },
-    { 4,1,3,1,1,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,4 },
-    { 4,1,1,1,1,1,1,1,2,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,4 },
-    { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
-    { 4,1,1,4,1,1,1,1,1,2,2,1,1,4,1,1,1,4,1,4,2,2,1,1,4 },
-    { 4,1,1,4,1,1,1,1,4,3,3,4,1,2,1,1,1,2,1,4,1,1,1,1,4 },
-    { 4,1,1,4,1,1,1,1,4,3,3,4,1,1,4,1,4,1,1,4,2,2,1,1,4 },
-    { 4,1,1,4,1,1,1,1,4,3,3,4,1,1,2,1,2,1,1,4,1,1,1,1,4 },
-    { 4,1,1,4,1,1,1,1,2,3,3,2,1,1,1,4,1,1,1,4,1,1,1,1,4 },
-    { 4,1,1,2,2,2,2,1,1,2,2,1,1,1,1,2,1,3,1,2,2,2,1,1,4 },
-    { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
-    { 4,1,1,1,1,1,1,1,1,2,1,1,1,1,2,2,4,1,1,1,1,1,1,1,4 },
-    { 4,1,1,1,1,1,1,1,4,3,4,1,1,1,1,1,2,1,1,1,1,1,1,1,4 },
-    { 4,1,1,3,1,1,1,1,2,3,2,1,1,1,1,2,1,1,1,1,1,1,1,1,4 },
-    { 4,1,1,1,1,1,1,1,1,2,1,1,2,1,2,1,1,1,1,1,1,1,3,1,4 },
-    { 4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,4 },
-    { 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2 }
-  }
+  local tileString = [[
+^#######################^
+^                    *  ^
+^  *                    ^
+^              *        ^
+^                       ^
+^    ##  ^##  ^## ^ ^   ^
+^   ^  ^ ^  ^ ^   ^ ^   ^
+^   ^  ^ ^ *# ^   ^ ^   ^
+^   ^  ^ ^##  ^## # #   ^
+^   ^  ^ ^  ^ ^    ^  * ^
+^ * ^  ^ ^  ^ ^    ^    ^
+^   #  # ^* # ^  * ^    ^
+^    ##  ###  ###  #    ^
+^                       ^
+^   *****************   ^
+^                       ^
+^  *                  * ^
+#########################
+]]
+
+  TileTable = {}
+
+  local width = #(tileString:match("[^\n]+"))
+
+  for x = 1,width,1 do TileTable[x] = {} end  
+
+  local rowIndex,columnIndex = 1,1
+  -- [^\n] means from the start of line to the end of line. gmatch iterates in all matches
+  for row in tileString:gmatch("[^\n]+") do
+    assert(#row == width, 'Map is not aligned: width of row ' .. tostring(rowIndex) .. ' should be ' .. tostring(width) .. ', but it is ' .. tostring(#row))
+    columnIndex = 1
+  -- gmatch(".") iterates in all characters that rows have
+    for character in row:gmatch(".") do
+      TileTable[columnIndex][rowIndex] = character
+      columnIndex = columnIndex + 1
+    end
+    rowIndex=rowIndex+1
+  end
 end
 
 function love.draw()
   -- Remember that uses ipars it demands more computer machine, so if you are experimenting
   -- performance issues, it should be better remove the ipairs and call the element inside manually
-  for rowIndex,row in ipairs(TileTable) do
-    for columnIndex,number in ipairs(row) do
+  for columnIndex,column in ipairs(TileTable) do
+    for rowIndex,char in ipairs(column) do
       local x,y = (columnIndex-1)*TileW, (rowIndex-1)*TileH
-      love.graphics.draw(Tileset, Quads[number], x, y)
+      love.graphics.draw(Tileset, Quads[char], x, y)
     end
   end
 end
