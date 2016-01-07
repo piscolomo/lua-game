2de626353bebf6102d414974aefb1b78c4743295
Julio Lopez <ljuliom@gmail.com>
2016-01-07 00:53:06 -0500

    cleaning hooks refactoring using functions

--- main.lua
+++ main.lua
@@ -1,20 +1,47 @@
-function love.load()
-  Tileset = love.graphics.newImage('countryside.png')
+function loadMap(tileW, tileH, tilesetPath, tileString, quadInfo)
   
-  TileW, TileH = 32, 32
+  TileW = tileW
+  TileH = tileH
+  Tileset = love.graphics.newImage(tilesetPath)
+  
   local tilesetW, tilesetH = Tileset:getWidth(), Tileset:getHeight()
-
-  local quadInfo = {
-    { ' ', 0,  0  }, -- grass 
-    { '#', 32,  0 }, -- box
-    { '*', 0, 32  }, -- flowers
-    { '^', 32, 32 }  -- boxTop
-  }
   Quads = {}
+  
   for _,info in ipairs(quadInfo) do
-    -- info[1] = character, info[2]= x, info[3] = y
-    Quads[info[1]] = love.graphics.newQuad(info[2], info[3], TileW, TileH, tilesetW, tilesetH)
+    -- info[1] = the character, info[2] = x, info[3] = y
+    Quads[info[1]] = love.graphics.newQuad(info[2], info[3], TileW,  TileH, tilesetW, tilesetH)
   end
+  
+  TileTable = {}
+  
+  local width = #(tileString:match("[^\n]+"))
+
+  for x = 1,width,1 do TileTable[x] = {} end
+
+  local rowIndex,columnIndex = 1,1
+  for row in tileString:gmatch("[^\n]+") do
+    assert(#row == width, 'Map is not aligned: width of row ' .. tostring(rowIndex) .. ' should be ' .. tostring(width) .. ', but it is ' .. tostring(#row))
+    columnIndex = 1
+    for character in row:gmatch(".") do
+      TileTable[columnIndex][rowIndex] = character
+      columnIndex = columnIndex + 1
+    end
+    rowIndex=rowIndex+1
+  end
+end
+
+function drawMap()
+  -- Remember that uses ipars it demands more computer machine, so if you are experimenting
+  -- performance issues, it should be better remove the ipairs and call the element inside manually
+  for columnIndex,column in ipairs(TileTable) do
+    for rowIndex,char in ipairs(column) do
+      local x,y = (columnIndex-1)*TileW, (rowIndex-1)*TileH
+      love.graphics.draw(Tileset, Quads[char], x, y)
+    end
+  end
+end
+
+function love.load()
 
   local tileString = [[
 ^#######################^
@@ -37,33 +64,16 @@ function love.load()
 #########################
 ]]
 
-  TileTable = {}
+  local quadInfo = {
+    { ' ', 0,  0  }, -- grass 
+    { '#', 32,  0 }, -- box
+    { '*', 0, 32  }, -- flowers
+    { '^', 32, 32 }  -- boxTop
+  }
 
-  local width = #(tileString:match("[^\n]+"))
-
-  for x = 1,width,1 do TileTable[x] = {} end  
-
-  local rowIndex,columnIndex = 1,1
-  -- [^\n] means from the start of line to the end of line. gmatch iterates in all matches
-  for row in tileString:gmatch("[^\n]+") do
-    assert(#row == width, 'Map is not aligned: width of row ' .. tostring(rowIndex) .. ' should be ' .. tostring(width) .. ', but it is ' .. tostring(#row))
-    columnIndex = 1
-  -- gmatch(".") iterates in all characters that rows have
-    for character in row:gmatch(".") do
-      TileTable[columnIndex][rowIndex] = character
-      columnIndex = columnIndex + 1
-    end
-    rowIndex=rowIndex+1
-  end
+  loadMap(32,32,'countryside.png',tileString,quadInfo)
 end
 
 function love.draw()
-  -- Remember that uses ipars it demands more computer machine, so if you are experimenting
-  -- performance issues, it should be better remove the ipairs and call the element inside manually
-  for columnIndex,column in ipairs(TileTable) do
-    for rowIndex,char in ipairs(column) do
-      local x,y = (columnIndex-1)*TileW, (rowIndex-1)*TileH
-      love.graphics.draw(Tileset, Quads[char], x, y)
-    end
-  end
+  drawMap()
 end
