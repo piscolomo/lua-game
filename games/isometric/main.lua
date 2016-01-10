ec663a59ea8f840da2a850196b74f0d65120dff7
Julio Lopez <ljuliom@gmail.com>
2016-01-10 15:31:54 -0500

    Game3: adding random blocks to the world

--- games/isometric/main.lua
+++ games/isometric/main.lua
@@ -1,6 +1,7 @@
 local bump = require "bump"
 
 local world = bump.newWorld()
+local cols_len = 0 -- how many collisions are happening
 
 local player = { x = 50, y = 50, w = 20, h = 20, speed = 80 }
 local blocks = {}
@@ -19,19 +20,49 @@ end
 
 
 -- Functions
+local function addBlock(x,y,w,h)
+  local block = {x=x,y=y,w=w,h=h}
+  blocks[#blocks+1] = block
+  world:add(block, x,y,w,h)
+end
+
 local function updatePlayer(dt)
-  
+end
+
+local function drawBlocks()
+  for _,block in ipairs(blocks) do
+    drawBox(block, 255,0,0)
+  end
 end
 
 -- Callbacks
 function love:load()
   world:add(player, player.x, player.y, player.w, player.h)
+
+  addBlock(0,       0,     800, 32)
+  addBlock(0,      32,      32, 600-32*2)
+  addBlock(800-32, 32,      32, 600-32*2)
+  addBlock(0,      600-32, 800, 32)
+
+  for i=1,30 do
+    addBlock( math.random(100, 600),
+              math.random(100, 400),
+              math.random(10, 100),
+              math.random(10, 100)
+    )
+  end
 end
 
 function love:update(dt)
-  updatePlayer(dt)
+  cols_len = 0
+  --updatePlayer(dt)
 end
 
 function love:draw()
+  drawBlocks()
   drawPlayer()
+end
+
+function love.keypressed(k)
+  if k == "escape" then love.event.quit() end
 end
